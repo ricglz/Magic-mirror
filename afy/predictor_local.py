1dafa25d9f59b94fd7268c7dff4734b55b0d3872
Ricardo J González C <rjgcastillo@hotmail.com>
2021-06-17 19:59:57 -0500

    Add new logs for predictor local

--- afy/predictor_local.py
+++ afy/predictor_local.py
@@ -8,11 +8,13 @@ import torch
 from torch.tensor import Tensor
 from torchvision.transforms.functional import to_pil_image
 
-from articulated.demo import load_checkpoints
-from articulated.animate import get_animation_region_params
-
 from afy.face_swap import swap_faces
 from afy.magic_mirror import MagicMirror
+from afy.utils import Logger
+from articulated.animate import get_animation_region_params
+from articulated.demo import load_checkpoints
+
+log = Logger('./var/log/predictor_local.log')
 
 def to_tensor(a: np.ndarray) -> Tensor:
     return Tensor(a[np.newaxis].astype(np.float32)).permute(0, 3, 1, 2)
@@ -24,6 +26,7 @@ def from_numpy_to_pil(array: np.ndarray):
     return Image.fromarray(array)
 
 def extract_face(image: Image.Image, box) -> Image.Image:
+    log('extracting face', important=True)
     margin = 25
     box[0] -= margin
     box[1] -= margin
@@ -32,12 +35,14 @@ def extract_face(image: Image.Image, box) -> Image.Image:
     return image.crop(box).resize((256, 256))
 
 def get_box_and_landmarks(image, mtcnn: MTCNN):
+    log('getting box and landmarks', important=True)
     box, _, landmarks = mtcnn.detect(image, True)
     box = box[0]
     landmarks = np.array(landmarks[0], np.int32)
     return box, landmarks
 
 def get_face(image_numpy: np.ndarray, mtcnn: MTCNN):
+    log('Getting face', important=True)
     with torch.no_grad():
         image = to_pil_image(to_tensor(image_numpy)[0]).resize((512, 512))
         box, landmarks = get_box_and_landmarks(image, mtcnn)
@@ -78,8 +83,10 @@ class PredictorLocal:
             source = source.to(self.device)
             source_img_data = driving_frame, landmarks
 
+            log('Calculating source region params', important=True)
             source_region_params = self.region_predictor(source)
 
+            log('Calculating new region params', important=True)
             new_region_params = get_animation_region_params(
                 self.driving_region_params,
                 source_region_params,
@@ -87,12 +94,15 @@ class PredictorLocal:
                 avd_network=self.avd_network,
                 mode='avd'
             )
+
+            log('Calculating modified face', important=True)
             modified_face = self.generator(
                 self.driving,
                 source_region_params=self.driving_region_params,
                 driving_region_params=new_region_params
             )['prediction'][0]
 
+            log('Doing face-swapping', important=True)
             modified_face_img = to_pil_image(modified_face).resize((512, 512))
             _, modified_landmarks = get_box_and_landmarks(modified_face_img, self.mtcnn)
             modified_img_data = np.array(modified_face_img), modified_landmarks
