4f5beb8355904ce6f9f779c2453d717fb84a7c89
Ricardo J González C <rjgcastillo@hotmail.com>
2021-07-01 16:59:45 -0500

    Organize better the test file for face swap

--- test_face_swap.py
+++ test_face_swap.py
@@ -3,6 +3,8 @@ import cv2
 
 from afy.face_swap_2 import Faceswap
 
+swapper = Faceswap()
+
 def annotate_landmarks(im, landmarks):
     im = im.copy()
     for idx, point in enumerate(landmarks):
@@ -21,26 +23,27 @@ def annotate_bboxes(im, bboxes):
         cv2.rectangle(im, pos_1, pos_2, color=(255, 0, 0))
     return im
 
-
-def main():
+def annotate_img():
     img_1 = cv2.imread('./avatars/opened_eyes.jpg')
-    # img_2 = cv2.imread('./avatars/closed_eyes.jpg')
-    swapper = Faceswap()
-
     # bboxes = swapper._get_bboxes(img_1)
     landmarks = swapper._get_landmarks(img_1)
 
     # annotated_img = annotate_bboxes(img_1, bboxes)
     annotated_img = annotate_landmarks(img_1, landmarks)
 
-    cv2.imwrite('annotated_img.jpg', annotated_img)
+    cv2.imshow('img_1', annotated_img)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
 
-    # cv2.imshow('img_1', annotated_img)
-    # cv2.waitKey(0)
-    # cv2.destroyAllWindows()
+def swap_imgs():
+    img_1 = cv2.imread('./avatars/opened_eyes.jpg')
+    img_2 = cv2.imread('./avatars/closed_eyes.jpg')
 
-    # swapped = swapper.faceswap(img_1, img_2)
-    # cv2.imwrite('swapped_face_2.jpg', swapped)
+    swapped = swapper.faceswap(img_1, img_2)
+    cv2.imwrite('swapped_face_2.jpg', swapped)
+
+def main():
+    swap_imgs()
 
 if __name__ == "__main__":
     main()
