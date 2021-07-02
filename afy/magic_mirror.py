4f8638763a0cf9e90322965c5d89f7cd3a561d0c
Ricardo J González C <rjgcastillo@hotmail.com>
2021-07-02 13:40:32 -0500

    Update magic mirror timers and how the module works

--- afy/magic_mirror.py
+++ afy/magic_mirror.py
@@ -6,18 +6,32 @@ from random import random
 from afy.utils import TicToc
 
 class State(Enum):
-    PREDICT = 0
-    WAIT_30 = 1
-    WAIT_5 = 2
+    '''State of the MagicMirror class to know how much to wait'''
+    WAIT_LONG = 0
+    WAIT_SHORT = 1
+    PREDICT = 2
+
+WAIT_TIMES = [15, 5, 5]
 
 class MagicMirror():
+    '''Module that manages the Magic Mirror experience'''
     def __init__(self):
-        self.state = State.WAIT_30
+        self.state = State.WAIT_LONG
         self.tic_toc = TicToc()
         self.tic_toc.tic()
 
+    @property
+    def wait_time(self):
+        '''Based on the current state returns how much time to wait'''
+        return WAIT_TIMES[self.state]
+
+    @property
+    def toc(self):
+        '''Get amount of seconds that is waiting since the timer started'''
+        return int(self.tic_toc.toc(seconds=True))
+
     def should_predict(self):
-        ''''''
+        '''Know if there should be a prediction. In addition to changing state'''
         if self.state == State.PREDICT:
             self._continue_predict()
             return True
@@ -26,20 +40,19 @@ class MagicMirror():
         return False
 
     def reset_tic(self):
+        '''Resets timer'''
         self.tic_toc.tic()
 
     def _continue_predict(self):
-        if int(self.tic_toc.toc(seconds=True)) == 5:
-            self.state = State.WAIT_30
+        if self.toc == self.wait_time:
+            self.state = State.WAIT_LONG
             self.tic_toc.tic()
 
     def _wait_time(self):
-        toc = int(self.tic_toc.toc(seconds=True))
-        if (self.state == State.WAIT_30 and toc == 30) or \
-           (self.state == State.WAIT_5 and toc == 5):
+        if self.toc == self.wait_time:
             prob = random()
             if prob <= 0.7:
                 self.state = State.PREDICT
             else:
-                self.state = State.WAIT_5
+                self.state = State.WAIT_SHORT
             self.tic_toc.tic()
