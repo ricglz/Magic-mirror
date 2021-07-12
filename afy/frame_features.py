a3aef377d1fe10c6cba589d33987b17b2430dc12
Ricardo J González C <rjgcastillo@hotmail.com>
2021-07-12 14:29:37 -0500

    [WIP] Continue to add the following steps for the inference of fsgan

---
+++ afy/frame_features.py
@@ -0,0 +1,40 @@
+'''Convinient object for managing frame features'''
+from typing import Tuple
+
+from face_alignment import FaceAlignment
+import numpy as np
+
+from afy.custom_typings import CV2Image
+from fsgan.utils.bbox_utils import get_main_bbox
+
+Features3D = Tuple[np.ndarray, np.ndarray, np.ndarray, np.ndarray]
+
+def get_3D_features(
+    rgb_frame: CV2Image,
+    aligner_2d: FaceAlignment,
+    aligner_3d: FaceAlignment,
+) -> Features3D:
+    detected_faces = aligner_2d.face_detector.detect_from_image(rgb_frame.copy())
+
+    # Skip current frame there if no faces were detected
+    if len(detected_faces) == 0:
+        return None
+    curr_bbox = get_main_bbox(np.array(detected_faces)[:, :4], rgb_frame.shape[:2])
+    detected_faces = [curr_bbox]
+
+    curr_landmarks = aligner_2d.get_landmarks(rgb_frame, detected_faces)[0]
+    curr_landmarks_3d = aligner_3d.get_landmarks(rgb_frame, detected_faces)[0]
+
+    # Convert bounding boxes format from [min, max] to [min, size]
+    curr_bbox[2:] = curr_bbox[2:] - curr_bbox[:2] + 1
+    return curr_bbox, curr_landmarks, curr_landmarks_3d
+
+class FrameFeatures():
+    def __init__(self, frame, aligner_2d, aligner_3d, img_transforms):
+        frame_rgb = frame[..., ::-1]
+        features = get_3D_features(frame_rgb, aligner_2d, aligner_3d)
+        transformed = img_transforms(frame_rgb, features[1], features[0])
+        self.tensor, self.landmarks_2d, self.bbox = transformed
+        _, self.landmarks_3d, _ = img_transforms(
+            frame_rgb, features[2], features[0]
+        )
--- afy/fsgan_predictor.py
+++ afy/fsgan_predictor.py
@@ -1,12 +1,16 @@
+'''Module containing predictor using fsgan model'''
 from typing import Tuple
 
 from face_alignment import FaceAlignment, LandmarksType
-import torch
 from torch.nn import Module
+import torch
 
+from afy.frame_features import FrameFeatures
+from afy.custom_typings import CV2Image
 from afy.predictor import Predictor
+
+# from fsgan.models.hopenet import Hopenet
 from fsgan.data import landmark_transforms
-from fsgan.models.hopenet import Hopenet
 from fsgan.utils.heatmap import LandmarkHeatmap
 from fsgan.utils.obj_factory import obj_factory
 
@@ -14,21 +18,21 @@ BLEND_MODEL_PATH = '../weights/ijbc_msrunet_256_2_0_blending_v1.pth'
 POSE_MODEL_PATH = '../weights/hopenet_robust_alpha1.pth'
 REENACTMENT_MODEL_PATH = '../weights/ijbc_msrunet_256_2_0_reenactment_v1.pth'
 
-pil_transforms1 = ('landmark_transforms.FaceAlignCrop', 'landmark_transforms.Resize(256)',
-                   'landmark_transforms.Pyramids(2)')
-pil_transforms2 = ('landmark_transforms.FaceAlignCrop', 'landmark_transforms.Resize(256)',
-                   'landmark_transforms.Pyramids(2)')
-tensor_transforms1 = ('landmark_transforms.ToTensor()',
-                      'transforms.Normalize(mean=[0.5,0.5,0.5],std=[0.5,0.5,0.5])')
-tensor_transforms2 = ('landmark_transforms.ToTensor()',
-                      'transforms.Normalize(mean=[0.5,0.5,0.5],std=[0.5,0.5,0.5])')
+PIL_TRANSFORMS = ('landmark_transforms.FaceAlignCrop', 'landmark_transforms.Resize(256)',
+                  'landmark_transforms.Pyramids(2)')
+TENSOR_TRANSFORMS = ('landmark_transforms.ToTensor()',
+                     'transforms.Normalize(mean=[0.5,0.5,0.5],std=[0.5,0.5,0.5])')
 
 def load_state_and_eval(model: Module, checkpoint: dict):
+    '''
+    Loads the state_dict contained in the checkpoint and set model to eval.
+    '''
     model.load_state_dict(checkpoint['state_dict'])
     model.eval()
     return model
 
 def img_transforms(pil_transforms: Tuple[str], tensor_transforms: Tuple[str]):
+    '''Create img_transforms based on pil and tensor transforms'''
     pil_transforms_arr = obj_factory(pil_transforms)
     tensor_transforms_arr = obj_factory(tensor_transforms)
     return landmark_transforms.ComposePyramids(
@@ -36,22 +40,30 @@ def img_transforms(pil_transforms: Tuple[str], tensor_transforms: Tuple[str]):
     )
 
 class FSGANPredictor(Predictor):
+    '''Predictor that uses a fsgan model as its backbone'''
+    source = None
+
     def __init__(self, *_):
         super().__init__()
-        self.aligner = FaceAlignment(LandmarksType._3D, flip_input=False)
+        self.aligner_2d = FaceAlignment(LandmarksType._2D, flip_input=False)
+        self.aligner_3d = FaceAlignment(LandmarksType._3D, flip_input=False)
         self.landmarks2heatmaps = LandmarkHeatmap().to(self.device)
-        self.gr = self._load_model(REENACTMENT_MODEL_PATH)
-        self.gb = self._load_model(BLEND_MODEL_PATH)
-        self.gp = self._load_hopenet(POSE_MODEL_PATH)
-        self.img_transforms1 = img_transforms(pil_transforms1, tensor_transforms1)
-        self.img_transforms2 = img_transforms(pil_transforms2, tensor_transforms2)
+        self.gen_r = self._load_model(REENACTMENT_MODEL_PATH)
+        self.gen_b = self._load_model(BLEND_MODEL_PATH)
+        # self.gen_p = self._load_hopenet(POSE_MODEL_PATH)
+        self.img_transforms = img_transforms(PIL_TRANSFORMS, TENSOR_TRANSFORMS)
 
     def _load_model(self, checkpoint_path: str):
         checkpoint: dict = torch.load(checkpoint_path)
         model: Module = obj_factory(checkpoint['arch']).to(self.device)
         return load_state_and_eval(model, checkpoint)
 
-    def _load_hopenet(self, checkpoint_path: str):
-        model = Hopenet().to(self.device)
-        checkpoint: dict = torch.load(checkpoint_path)
-        return load_state_and_eval(model, checkpoint)
+    # def _load_hopenet(self, checkpoint_path: str):
+    #     model = Hopenet().to(self.device)
+    #     checkpoint: dict = torch.load(checkpoint_path)
+    #     return load_state_and_eval(model, checkpoint)
+
+    def set_source_image(self, source_image: CV2Image):
+        self.source = FrameFeatures(
+            source_image, self.aligner_2d, self.aligner_3d, self.img_transforms
+        )
