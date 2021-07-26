760e2802351a91247d23b77a100611cbc6f7364c
Ricardo J González C <rjgcastillo@hotmail.com>
2021-07-26 11:31:53 -0500

    Fix issue of legacy argument options

--- run_windows.bat
+++ run_windows.bat
@@ -4,4 +4,4 @@ call scripts/settings_windows.bat
 
 call conda activate %CONDA_ENV_NAME%
 
-call python afy/cam_fomm.py --config %CONFIG% --relative --adapt_scale --no-pad --checkpoint %CKPT% %*
+call python afy/cam_fomm.py --config %CONFIG% --checkpoint %CKPT% %*
