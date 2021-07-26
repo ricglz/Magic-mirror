f78d44dbebea1b3261dc877c04a048bbc2f5ebc3
Ricardo J González C <rjgcastillo@hotmail.com>
2021-07-26 14:49:18 -0500

    Fix another typo

--- run_experiment_windows.bat
+++ run_experiment_windows.bat
@@ -4,12 +4,12 @@ md output
 md output/%2
 
 REM Articulated (No face-swap)
-call run_local_windows.bat --input-video videos/%1 --ouput output/%2/articulated.mp4
+call run_local_windows.bat --input-video videos/%1 --output output/%2/articulated.mp4
 REM Articulated (Poisson)
-call run_local_windows.bat --input-video videos/%1 --ouput output/%2/poisson.mp4 --swap-face --swapper poisson
+call run_local_windows.bat --input-video videos/%1 --output output/%2/poisson.mp4 --swap-face --swapper poisson
 REM Articulated (EDS)
-call run_local_windows.bat --input-video videos/%1 --ouput output/%2/eds.mp4 --swap-face --swapper eds
+call run_local_windows.bat --input-video videos/%1 --output output/%2/eds.mp4 --swap-face --swapper eds
 REM Articulated (Triangulation)
-call run_local_windows.bat --input-video videos/%1 --ouput output/%2/triangulation.mp4 --swap-face --swapper triangulation
+call run_local_windows.bat --input-video videos/%1 --output output/%2/triangulation.mp4 --swap-face --swapper triangulation
 REM FSGAN
-call run_local_windows.bat --input-video videos/%1 --ouput output/%2/fsgan.mp4 --fsgan
+call run_local_windows.bat --input-video videos/%1 --output output/%2/fsgan.mp4 --fsgan
