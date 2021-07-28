ad2c207692ed14eb2421e5920128a400c0e1e4e5
Ricardo J González C <rjgcastillo@hotmail.com>
2021-07-28 07:43:17 -0500

    Just run fsgan for now

--- run_experiment_windows.bat
+++ run_experiment_windows.bat
@@ -4,12 +4,12 @@ echo %1
 echo %2
 
 echo Articulated (No face-swap)
-call run_local_windows.bat --input-video videos/%1 --output output/%2/articulated
+:: call run_local_windows.bat --input-video videos/%1 --output output/%2/articulated
 echo Articulated (Poisson)
-call run_local_windows.bat --input-video videos/%1 --output output/%2/poisson --swap-face --swapper poisson
+:: call run_local_windows.bat --input-video videos/%1 --output output/%2/poisson --swap-face --swapper poisson
 echo Articulated (EDS)
-call run_local_windows.bat --input-video videos/%1 --output output/%2/eds --swap-face --swapper eds
+:: call run_local_windows.bat --input-video videos/%1 --output output/%2/eds --swap-face --swapper eds
 echo Articulated (Triangulation)
-call run_local_windows.bat --input-video videos/%1 --output output/%2/triangulation --swap-face --swapper triangulation
+:: call run_local_windows.bat --input-video videos/%1 --output output/%2/triangulation --swap-face --swapper triangulation
 echo FSGAN
 call run_local_windows.bat --input-video videos/%1 --output output/%2/fsgan --fsgan
