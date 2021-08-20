4a901e20c3e4bc9347cb4d8470ce0974a1394cd8
Ricardo J González C <26823011+ricglz@users.noreply.github.com>
2021-08-20 09:43:36 -0500

    Add resolution also as a variable

--- run_experiment_windows.bat
+++ run_experiment_windows.bat
@@ -3,14 +3,15 @@
 echo %1
 echo %2
 echo %3
+echo %4
 
 echo Articulated (No face-swap)
-call run_local_windows.bat --resolution 512 --input-video videos/%1 --output output/%2/%3/articulated
+call run_local_windows.bat --resolution %4 --input-video videos/%1 --output output/%4/%2/%3/articulated
 echo Articulated (Poisson)
-call run_local_windows.bat --resolution 512 --input-video videos/%1 --output output/%2/%3/poisson --swap-face --swapper poisson
+call run_local_windows.bat --resolution %4 --input-video videos/%1 --output output/%4/%2/%3/poisson --swap-face --swapper poisson
 echo Articulated (EDS)
-call run_local_windows.bat --resolution 512 --input-video videos/%1 --output output/%2/%3/eds --swap-face --swapper eds
+call run_local_windows.bat --resolution %4 --input-video videos/%1 --output output/%4/%2/%3/eds --swap-face --swapper eds
 echo Articulated (Triangulation)
-call run_local_windows.bat --resolution 512 --input-video videos/%1 --output output/%2/%3/triangulation --swap-face --swapper triangulation
+call run_local_windows.bat --resolution %4 --input-video videos/%1 --output output/%4/%2/%3/triangulation --swap-face --swapper triangulation
 echo FSGAN
-call run_local_windows.bat --resolution 512 --input-video videos/%1 --output output/%2/%3/fsgan --fsgan
+call run_local_windows.bat --resolution %4 --input-video videos/%1 --output output/%4/%2/%3/fsgan --fsgan
--- run_experiments_windows.bat
+++ run_experiments_windows.bat
@@ -7,20 +7,32 @@ set VIDEO_4=ellbat.mp4
 set VIDEO_5=healthy_gamer.mp4
 set VIDEO_6=javier.mp4
 
-call run_experiment_windows.bat %VIDEO_1% with_glasses 0
-call run_experiment_windows.bat %VIDEO_1% with_glasses 1
+call run_experiment_windows.bat %VIDEO_1% with_glasses 0 256
+call run_experiment_windows.bat %VIDEO_1% with_glasses 1 256
+call run_experiment_windows.bat %VIDEO_1% with_glasses 0 512
+call run_experiment_windows.bat %VIDEO_1% with_glasses 1 512
 
-call run_experiment_windows.bat %VIDEO_2% without_glasses 0
-call run_experiment_windows.bat %VIDEO_2% without_glasses 1
+call run_experiment_windows.bat %VIDEO_2% without_glasses 0 256
+call run_experiment_windows.bat %VIDEO_2% without_glasses 1 256
+call run_experiment_windows.bat %VIDEO_2% without_glasses 0 512
+call run_experiment_windows.bat %VIDEO_2% without_glasses 1 512
 
-call run_experiment_windows.bat %VIDEO_3% crystal 0
-call run_experiment_windows.bat %VIDEO_3% crystal 1
+call run_experiment_windows.bat %VIDEO_3% crystal 0 256
+call run_experiment_windows.bat %VIDEO_3% crystal 1 256
+call run_experiment_windows.bat %VIDEO_3% crystal 0 512
+call run_experiment_windows.bat %VIDEO_3% crystal 1 512
 
-call run_experiment_windows.bat %VIDEO_4% ellbat 0
-call run_experiment_windows.bat %VIDEO_4% ellbat 1
+call run_experiment_windows.bat %VIDEO_4% ellbat 0 256
+call run_experiment_windows.bat %VIDEO_4% ellbat 1 256
+call run_experiment_windows.bat %VIDEO_4% ellbat 0 512
+call run_experiment_windows.bat %VIDEO_4% ellbat 1 512
 
-call run_experiment_windows.bat %VIDEO_5% healthy_gamer 0
-call run_experiment_windows.bat %VIDEO_5% healthy_gamer 1
+call run_experiment_windows.bat %VIDEO_5% healthy_gamer 0 256
+call run_experiment_windows.bat %VIDEO_5% healthy_gamer 1 256
+call run_experiment_windows.bat %VIDEO_5% healthy_gamer 0 512
+call run_experiment_windows.bat %VIDEO_5% healthy_gamer 1 512
 
-call run_experiment_windows.bat %VIDEO_6% javier 0
-call run_experiment_windows.bat %VIDEO_6% javier 1
+call run_experiment_windows.bat %VIDEO_6% javier 0 256
+call run_experiment_windows.bat %VIDEO_6% javier 1 256
+call run_experiment_windows.bat %VIDEO_6% javier 0 512
+call run_experiment_windows.bat %VIDEO_6% javier 1 512
