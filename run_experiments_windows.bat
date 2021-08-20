06deaa53f057da1457f28e8af11b10a9c9f901ed
Ricardo J González C <26823011+ricglz@users.noreply.github.com>
2021-08-20 09:26:32 -0500

    Add new aspects for the experiments

--- run_experiments_windows.bat
+++ run_experiments_windows.bat
@@ -2,6 +2,25 @@
 
 set VIDEO_1=with_glasses.mp4
 set VIDEO_2=without_glasses.mp4
+set VIDEO_3=crystal.mp4
+set VIDEO_4=ellbat.mp4
+set VIDEO_5=healthy_gamer.mp4
+set VIDEO_6=javier.mp4
 
-call run_experiment_windows.bat %VIDEO_1% video_1
-call run_experiment_windows.bat %VIDEO_2% video_2
+call run_experiment_windows.bat %VIDEO_1% with_glasses 0
+call run_experiment_windows.bat %VIDEO_1% with_glasses 1
+
+call run_experiment_windows.bat %VIDEO_2% without_glasses 0
+call run_experiment_windows.bat %VIDEO_2% without_glasses 1
+
+call run_experiment_windows.bat %VIDEO_3% crystal 0
+call run_experiment_windows.bat %VIDEO_3% crystal 1
+
+call run_experiment_windows.bat %VIDEO_4% ellbat 0
+call run_experiment_windows.bat %VIDEO_4% ellbat 1
+
+call run_experiment_windows.bat %VIDEO_5% healthy_gamer 0
+call run_experiment_windows.bat %VIDEO_5% healthy_gamer 1
+
+call run_experiment_windows.bat %VIDEO_6% javier 0
+call run_experiment_windows.bat %VIDEO_6% javier 1
